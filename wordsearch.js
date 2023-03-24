febecf9599932ffe7afd519dd7bd5c6aba02a9c3
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-24 12:37:32 -0700

    Add horizontal and vertical word checks using matrix transposition

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  // return false if letters is an empty array
+  if (!letters.length) {
+    return false;
+  }
+  // assuming a horizontal orientation
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (let i = 0; i < horizontalJoin.length; i++) {
+    if (horizontalJoin[i] === word) {
+      return true;
     }
-}
+  }
+  // assuming a vertical orientation, transpose the matrix
+  const verticalArray = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    verticalArray.push([]);
+  }
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      verticalArray[j].push(letters[i][j]);
+    }
+  }
 
-module.exports = wordSearch
+  const verticalJoin = verticalArray.map((ls) => ls.join(""));
+  for (let i = 0; i < verticalJoin.length; i++) {
+    if (verticalJoin[i] === word) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
